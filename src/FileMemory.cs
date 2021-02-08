ec505823a00451aa04a0f10be38923f1cb21c4a6
dimohy <dimohy01@gmail.com>
2021-02-08 13:02:58 +0900

    초기 커밋.

---
+++ src/FileMemory.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+
+
+namespace Dimohysm
+{
+    public sealed class FileMemory : IDisposable
+    {
+        private MemoryMappedFile mmf;
+        private MemoryMappedViewAccessor accessor;
+        private readonly unsafe byte* memoryMappedAddress;
+        private readonly long fileLength;
+
+        private bool disposedValue;
+
+
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    accessor.SafeMemoryMappedViewHandle.ReleasePointer();
+                    accessor.Dispose();
+                    mmf.Dispose();
+                }
+
+                accessor = null;
+                mmf = null;
+                
+                disposedValue = true;
+            }
+        }
+
+        ~FileMemory()
+        {
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public unsafe Span<byte> AsBytes()
+        {
+            return new Span<byte>(memoryMappedAddress, (int)fileLength);
+        }
+
+        public unsafe ReadOnlySpan<byte> AsReadOnlyBytes()
+        {
+            return new ReadOnlySpan<byte>(memoryMappedAddress, (int)fileLength);
+        }
+
+        private FileMemory(string filepath)
+        {
+            fileLength = new FileInfo(filepath).Length;
+            mmf = MemoryMappedFile.CreateFromFile(filepath);
+            accessor = mmf.CreateViewAccessor();
+            
+            unsafe
+            {
+                accessor.SafeMemoryMappedViewHandle.AcquirePointer(ref memoryMappedAddress);
+                memoryMappedAddress += accessor.PointerOffset;
+            }
+        }
+
+        public static FileMemory Connect(string filepath)
+        {
+            return new FileMemory(filepath);
+        }
+    }
+}
---
+++ test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Dimohysm.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "./temp.dat";
+
+            CreateTempFile(filename);
+
+            using var mmfSpan = FileMemory.Connect(filename);
+            var span = mmfSpan.AsReadOnlyBytes();
+            var sum = 0L;
+
+            var sw = Stopwatch.StartNew();
+            for (var count = 1; count <= 100; count++)
+            {
+                for (var i = 0; i < span.Length; i++)
+                {
+                    sum += span[i];
+                }
+                Console.WriteLine($"Count: {count}");
+            }
+            sw.Stop();
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Elapsed Time: {sw.ElapsedMilliseconds}");
+
+
+            static void CreateTempFile(string filename)
+            {
+                if (File.Exists(filename) == true)
+                    return;
+                
+                using var fs = File.OpenWrite(filename);
+                var length = 1000 * 1000 * 1000;
+                for (var i = 0; i < length; i++)
+                    fs.WriteByte((byte)i);
+            }
+        }
+    }
+}
